7cb57bf95c50517f7d5559484c76da7f02162082
juniehyun <juniehyun@gmail.com>
2016-11-30 15:21:04 -0800

    start to-do list

---
+++ components/todo-list/todo-list.js
@@ -0,0 +1,11 @@
+/**
+ * Created by JunieHyun on 11/30/16.
+ */
+function todoListController(){
+
+}
+
+angular.module('tdlApp').component('todoList',{
+   templateUrl: './components/todo-list/todo-list.html',
+    controller: todoListController()g
+});
---
+++ script.js
@@ -0,0 +1,4 @@
+/**
+ * Created by JunieHyun on 11/30/16.
+ */
+angular.module('tdlApp',[]);
